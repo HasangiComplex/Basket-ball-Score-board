1085b4644a9af581fecf12a29d8c199461dc2a0c
hansika_112197 <hansika.piumali@axiatadigitallabs.com>
2024-05-20 21:50:13 +0530

    Added functionality to add scores and update

---
+++ score.js
@@ -0,0 +1,24 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+
+document.getElementById("addOne").addEventListener("click",()=>{
+
+    let displayVal =parseInt(homeScore.textContent)
+    displayVal += 1 ;
+    homeScore.textContent = displayVal
+})
+
+function addScoresForHome(points) {
+    let displayScoreHome =parseInt(homeScore.textContent)
+    displayScoreHome+= points;
+    homeScore.textContent = displayScoreHome
+
+}
+
+function addScoresForGuest(points) {
+    let displayScoreGuest =parseInt(guestScore.textContent)
+    displayScoreGuest+= points;
+    guestScore.textContent = displayScoreGuest
+}
+
